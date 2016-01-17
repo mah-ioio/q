dfa294d4c8b8db2365accd1a7d148fb4b29b8cbf
marcus johansson <mj.emeth@gmail.com>
2016-01-17 02:27:23 +0100

    Queue version 1

--- app/scripts/screen.js
+++ app/scripts/screen.js
@@ -60,10 +60,10 @@ var screen = (function(win, doc, Firebase, undefined){
 		$queue.empty();
 		for(var i = 0; i < queue.length; i++){
 			var no = i+1;
-			var name = '<strong>'+ no + '</strong>' + " " + queue[i].user.info.first_name +" "+ queue[i].user.info.last_name;
+			var name = '<table><tr><td rowspan="2"><strong>'+ no + '</strong></td><td class="list-group-center"><h2>' + queue[i].user.info.first_name +" "+ queue[i].user.info.last_name+'<h2></td></tr>';
 			var est = queue[i].ticket.est + "";
 			var location = queue[i].ticket.location + "";
-			$queue.append('<li class="list-group-item">' + name +" "+'<span>'+est+"min @ <em>"+location+'</em></span></li>');
+			$queue.append('<li class="list-group-item">' + name +" "+'<tr><td class="list-group-center"><h2><span>'+est+"min @ <em>"+location+'</em></span><h2></td></tr></table></li>');
 		}
 	};
 
