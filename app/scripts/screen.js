7703bb5df4a10927aae94ba52e805c3e12d5a124
marcus johansson <mj.emeth@gmail.com>
2016-01-19 14:53:53 +0100

    Added studies

--- app/scripts/screen.js
+++ app/scripts/screen.js
@@ -61,7 +61,7 @@ var screen = (function(win, doc, Firebase, undefined){
 		for(var i = 0; i < queue.length; i++){
 			
 			var no = i+1;
-			var name = '<table><tr><td rowspan="2"><strong>'+ no + '</strong></td><td class="list-group-center"><h2>' + queue[i].user.info.first_name +" "+ queue[i].user.info.last_name+'<h2></td></tr>';
+			var name = '<table><tr><td rowspan="2"><strong>'+ no + '</strong></td><td class="list-group-center"><h2>' + queue[i].user.info.first_name +" "+ queue[i].user.info.last_name+", "+ queue[i].user.info.studies+'<h2></td></tr>';
 			var est = queue[i].ticket.est + "";
 			var location = queue[i].ticket.location + "";
 			if (i<=4){
